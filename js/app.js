b85baddca40b7143c7d4f3c024c1d71f0cf634db
why <381421746@qq.com>
2017-03-25 09:54:08 +0800

    first push

---
+++ js/app.js
@@ -0,0 +1,188 @@
+/**
+ * 演示程序当前的 “注册/登录” 等操作，是基于 “本地存储” 完成的
+ * 当您要参考这个演示程序进行相关 app 的开发时，
+ * 请注意将相关方法调整成 “基于服务端Service” 的实现。
+ **/
+(function($, owner) {
+	/**
+	 * 用户登录
+	 **/
+	owner.login = function(loginInfo, callback) {
+		console.log('login..............................................');
+		ws = new WebSocket("ws://"+"192.168.1.100"+":7272");
+		callback = callback || $.noop;
+		loginInfo = loginInfo || {};
+		loginInfo.account = loginInfo.account || '';
+		loginInfo.password = loginInfo.password || '';
+		
+		//		if(loginInfo.account.length < 5) {
+		//			return callback('账号最短为 5 个字符');
+		//		}
+		//		if(loginInfo.password.length < 6) {
+		//			return callback('密码最短为 6 个字符');
+		//		}
+		
+//		return callback();
+		
+//		var issuccess;
+console.log('sss');
+		mui.post('http://192.168.1.100/ychat/ychat-server/index.php/W/li', loginInfo, function(data) {
+			console.log("dddd");
+			console.log(JSON.stringify(data));
+			console.log(JSON.stringify(data['0']['idWhyUser']));
+			console.log(data.success);
+			console.log(data['0']['idWhyUser']);
+			
+			if(data.success) {
+				
+				localStorage.setItem('token',data.token);
+				localStorage.setItem('uid',data['0']['idWhyUser']);
+				
+				return owner.createState(loginInfo.account, callback);
+			} else {
+				console.log("hhhhhh")
+				return callback('用户名或密码错误');
+			}
+			//			alert('ha?');
+
+		}, 'json');
+		//		alert('ha?hh');
+
+		//		var users = JSON.parse(localStorage.getItem('$users') || '[]');
+		//		var authed = users.some(function(user) {
+		//			return loginInfo.account == user.account && loginInfo.password == user.password;
+		//		});
+
+	};
+
+	owner.createState = function(name, callback) {
+		var state = owner.getState();
+		state.account = name;
+		state.token = "token123456789";
+		owner.setState(state);
+		return callback();
+	};
+
+	/**
+	 * 新用户注册
+	 **/
+	owner.reg = function(regInfo, callback) {
+		callback = callback || $.noop; //var attr = attr || “”;这个运算经常用来判断一个变量是否已定义，如果没有定义就给他一个初始值，这在给函数的参数定义一个默认值的时候比较有用。因为js不像php可以直接在型参数上定义func($attr=5)。再次提醒你记住上面的原则：如果实参需要是0、""、null、false、undefined、NaN的时候也会当false来处理。
+		regInfo = regInfo || {};
+		regInfo.account = regInfo.account || '';
+		regInfo.password = regInfo.password || '';
+		//		if(regInfo.account.length < 5) {
+		//			return callback('用户名最短需要 5 个字符');
+		//		}
+		//		if(regInfo.password.length < 6) {
+		//			return callback('密码最短需要 6 个字符');
+		//		}
+		//		if(!checkEmail(regInfo.email)) {
+		//			return callback('邮箱地址不合法');
+		//		}
+
+		/*
+		 * 这里改成向服务器发送
+		 */
+		//要有'http://'
+		mui.post('http://192.168.1.100/whyChat/index.php/W/lo', regInfo, function(data) {
+			//							console.log(data);
+			console.log(JSON.stringify(data));
+			//			alert('ha?');
+		}, 'json');
+
+		//		var users = JSON.parse(localStorage.getItem('$users') || '[]'); //为空则设置值（就是之前没有）
+		//		users.push(regInfo);
+		//		localStorage.setItem('$users', JSON.stringify(users));
+		
+		return callback();
+	};
+
+	/**
+	 * 获取当前状态
+	 **/
+	owner.getState = function() {
+		
+		var stateText = localStorage.getItem('$state') || "{}";
+		return JSON.parse(stateText);
+		
+	};
+
+	/**
+	 * 设置当前状态
+	 **/
+	owner.setState = function(state) {
+		state = state || {};
+		localStorage.setItem('$state', JSON.stringify(state));
+		//var settings = owner.getSettings();
+		//settings.gestures = '';
+		//owner.setSettings(settings);
+	};
+
+	var checkEmail = function(email) {
+		email = email || '';
+		return(email.length > 3 && email.indexOf('@') > -1);
+	};
+
+	/**
+	 * 找回密码
+	 **/
+	owner.forgetPassword = function(email, callback) {
+		callback = callback || $.noop;
+		if(!checkEmail(email)) {
+			return callback('邮箱地址不合法');
+		}
+		return callback(null, '新的随机密码已经发送到您的邮箱，请查收邮件。');
+	};
+
+	/**
+	 * 获取应用本地配置
+	 **/
+	owner.setSettings = function(settings) {
+		settings = settings || {};
+		localStorage.setItem('$settings', JSON.stringify(settings));
+	}
+
+	/**
+	 * 设置应用本地配置
+	 **/
+	owner.getSettings = function() {
+			var settingsText = localStorage.getItem('$settings') || "{}";
+			return JSON.parse(settingsText);
+		}
+		/**
+		 * 获取本地是否安装客户端
+		 **/
+	owner.isInstalled = function(id) {
+		if(id === 'qihoo' && mui.os.plus) {
+			return true;
+		}
+		if(mui.os.android) {
+			var main = plus.android.runtimeMainActivity();
+			var packageManager = main.getPackageManager();
+			var PackageManager = plus.android.importClass(packageManager)
+			var packageName = {
+				"qq": "com.tencent.mobileqq",
+				"weixin": "com.tencent.mm",
+				"sinaweibo": "com.sina.weibo"
+			}
+			try {
+				return packageManager.getPackageInfo(packageName[id], PackageManager.GET_ACTIVITIES);
+			} catch(e) {}
+		} else {
+			switch(id) {
+				case "qq":
+					var TencentOAuth = plus.ios.import("TencentOAuth");
+					return TencentOAuth.iphoneQQInstalled();
+				case "weixin":
+					var WXApi = plus.ios.import("WXApi");
+					return WXApi.isWXAppInstalled()
+				case "sinaweibo":
+					var SinaAPI = plus.ios.import("WeiboSDK");
+					return SinaAPI.isWeiboAppInstalled()
+				default:
+					break;
+			}
+		}
+	}
+}(mui, window.app = {}));
---
+++ js/wsconnect.js
@@ -0,0 +1,82 @@
+
+var reconntimes = 0;
+
+// 连接服务端
+function connect() {
+
+	console.log("建立WS链接");
+
+	// 创建websocket
+	ws = new WebSocket("ws://" + "192.168.1.100" + ":7272");
+
+	// 当socket连接打开时，输入用户名
+	ws.onopen = onopen;
+	// 当有消息时根据消息类型显示不同信息
+	ws.onmessage = onmessage;
+	ws.onclose = function() {
+						
+		console.log("连接关闭，定时重连");
+		
+		if(reconntimes<5)
+		setTimeout(connect(),5000);
+		else{
+			console.log("已超过自动重连次数。")
+		}
+		connect();
+	};
+	ws.onerror = function() {
+		console.log("出现错误");
+	};
+}
+
+// 连接建立时发送登录信息
+function onopen() {
+	reconntimes =0;
+
+	//    var login_data = '{"type":"login","client_name":"'+name.replace("", '\\"')+'","room_id":"<?php echo isset($_GET['room_id']) ? $_GET['room_id'] : 1?>"}';
+	//     var login_data ={};
+	//     console.log("websocket握手成功，发送登录数据:"+login_data);
+
+	var login_data = { "type": "login", "uid": localStorage.getItem('uid'), "token": localStorage.getItem('token') };
+	console.log("onopen");
+	ws.send(JSON.stringify(login_data));
+
+}
+
+// 服务端发来消息时
+function onmessage(e) {
+	console.log(e.data);
+	//      var data = eval("("+e.data+")");
+	//      switch(data['type']){
+	//          // 服务端ping客户端
+	//          case 'ping':
+	//              ws.send('{"type":"pong"}');
+	//              break;;
+	//          // 登录 更新用户列表
+	//          case 'login':
+	//              //{"type":"login","client_id":xxx,"client_name":"xxx","client_list":"[...]","time":"xxx"}
+	//              say(data['client_id'], data['client_name'],  data['client_name']+' 加入了聊天室', data['time']);
+	//              if(data['client_list'])
+	//              {
+	//                  client_list = data['client_list'];
+	//              }
+	//              else
+	//              {
+	//                  client_list[data['client_id']] = data['client_name']; 
+	//              }
+	//              flush_client_list();
+	//              console.log(data['client_name']+"登录成功");
+	//              break;
+	//          // 发言
+	//          case 'say':
+	//              //{"type":"say","from_client_id":xxx,"to_client_id":"all/client_id","content":"xxx","time":"xxx"}
+	//              say(data['from_client_id'], data['from_client_name'], data['content'], data['time']);
+	//              break;
+	//          // 用户退出 更新用户列表
+	//          case 'logout':
+	//              //{"type":"logout","client_id":xxx,"time":"xxx"}
+	//              say(data['from_client_id'], data['from_client_name'], data['from_client_name']+' 退出了', data['time']);
+	//              delete client_list[data['from_client_id']];
+	//              flush_client_list();
+	//      }
+}
